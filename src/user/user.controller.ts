3b4c925c3306e293a8199e7b4292c0175c4f2553
rivki <r0533145417@gmail.com>
2022-08-08 10:39:24 +0300

    create user-module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { UserService } from './user.service';
-
+import { User } from './user.model';
 @Controller('user')
 export class UserController {
+
   constructor(private userService: UserService) { }
+
+  @Post()
+  createUser(@Body() newUser: User) {
+    this.userService.createUser(newUser);
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') userId: string, @Body() updateUser: User) {
+    this.userService.updateUser(userId, updateUser);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') userId: string) {
+    this.userService.deleteUser(userId);
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') userId: string) {
+    return this.userService.getUserById(userId);
+  }
 
   @Get()
   getAll() {
-    return [{ id: 1, 'name': 'root', 'email': 'root@example.com' }, { id: 2, 'name': 'tamar', 'email': 'tamar@example.com' }];
+    return this.userService.getUsers();
   }
+
 }
